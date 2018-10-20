79259852eedebb1cf2c90b5c2ec9d3da89d69f8e
rjmcbain <rmcbain01@gmail.com>
2018-10-20 15:43:22 -0600

    adding html and js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,24 @@
 //   })
 //   .change();
 
-$("#inventoryForm")
-  .change(function() {
-    $(".box.b").css("background-color", "red");
+// $("#inventoryForm")
+//   .change(function() {
+//     $(".box.b").css("background-color", "red");
+//   })
+//   .change();
+
+$("#inventoryForm").on("click", function() {
+  $(".textvalue").show();
+});
+
+$("select")
+  .on("change", function() {
+    $(".textvalue div")
+      .removeClass("highlight")
+      .filter(":contains(" + this.value + ")")
+      .addClass("highlight");
+    if (this.value === "1") {
+      console.log("Not Available");
+    }
   })
   .change();
